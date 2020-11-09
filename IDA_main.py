f08ac15fa052fd84363387049f1625ad29ae5cca
ascherf-ml <alexander-scherf@gmx.net>
2020-11-09 15:40:28 +0100

    update

--- IDA_main.py
+++ IDA_main.py
@@ -31,46 +31,7 @@ from modules.speechprocessing import *
 # Voice output settings
 # =============================================================================
 
-
-
 wiki_wiki = wikipediaapi.Wikipedia('en')
-
-# =============================================================================
-# Speech library
-# =============================================================================
-
-morning_greet   = ['Good Morning sunshine', 'good morning', 'morning', 'guten tag',
-                   'Rise and shine', 'Wakey, wakey, eggs and bakey', 'Good morning, Sleeping Beauty',
-                   'I thought you’d never wake up', 'Morning mi amigo!', 'Top o the mornin to ya', 'Rise and shine, its time for wine',
-                   'Good morning do you have coffee already', 'how was your night']
-midday_greet    = ['Nice to hear from you', 'good to hear you',
-                   'nice to hear you', 'its been a while']
-afternoon_greet = ['Good Afternoon']
-evening_greet   = ['Good Evening']
-night_greet     = ['You are still up?']
-greet_followers = ['anything else?', 'do you need help with something else?',
-                   'what else can i do', 'do you need help with something different',
-                   'Im ready if you are']
-greet_questions = ['Do you need something from me?', 'Is there something I can help you with?',
-                   'What are we doing today?', 'Do you need help?', 'You need something today?',
-                   'What are our plans for today', 'Do you have some exiting tasks for me?', 'Let me brighten your day',
-                   'Shoot']
-greetings       = ['hi', 'hey', 'hello', 'whats up', 'hows it going',
-                   'how are you', 'how have you been']
-
-# create library and add phrases
-phrases = {}
-phrases["morning_greet"] = (morning_greet)
-phrases["midday_greet"] = (midday_greet)
-phrases["afternoon_greet"] = (afternoon_greet)
-phrases["evening_greet"] = (evening_greet)
-phrases["night_greet"] = (night_greet)
-phrases["greet_followers"] = (greet_followers)
-phrases["greet_questions"] = (greet_questions)
-phrases["greetings"] = (greetings)
-
-# set user name
-user = "alex"
 
 # =============================================================================
 # IDA Databases
@@ -87,7 +48,7 @@ main_data
 # =============================================================================
 # IDA functions
 # =============================================================================
-speak('hello')
+speak('hallo')
 
 webbrowser.register('firefox', None, webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
 
--- modules/speechprocessing.py
+++ modules/speechprocessing.py
@@ -20,7 +20,7 @@ def takeCommand():
         audio = r.listen(source)
 
         try:
-            statement = r.recognize_google(audio, language='en-EN')
+            statement = r.recognize_google(audio, language='de-DE')
 
         except Exception as e:
             #speak(f"Excuse me {user}, i didn't get that")
@@ -31,6 +31,7 @@ def wishMe():
     global main_data
     global main_data_columns
     hour = datetime.datetime.now().hour
+    engine = speech_engine()
     if hour >= 0 and hour < 10:
         sentence=(
             f" {random.choice(phrases['greetings'])} {user}. {random.choice(phrases['morning_greet'])}")
@@ -74,3 +75,36 @@ def speech_engine():
     engine.setProperty('voice', voices[0].id)
 
     return engine
+
+morning_greet   = ['Good Morning sunshine', 'good morning', 'morning', 'guten tag',
+                   'Rise and shine', 'Wakey, wakey, eggs and bakey', 'Good morning, Sleeping Beauty',
+                   'I thought you’d never wake up', 'Morning mi amigo!', 'Top o the mornin to ya', 'Rise and shine, its time for wine',
+                   'Good morning do you have coffee already', 'how was your night']
+midday_greet    = ['Nice to hear from you', 'good to hear you',
+                   'nice to hear you', 'its been a while']
+afternoon_greet = ['Good Afternoon']
+evening_greet   = ['Good Evening']
+night_greet     = ['You are still up?']
+greet_followers = ['anything else?', 'do you need help with something else?',
+                   'what else can i do', 'do you need help with something different',
+                   'Im ready if you are']
+greet_questions = ['Do you need something from me?', 'Is there something I can help you with?',
+                   'What are we doing today?', 'Do you need help?', 'You need something today?',
+                   'What are our plans for today', 'Do you have some exiting tasks for me?', 'Let me brighten your day',
+                   'Shoot']
+greetings       = ['hi', 'hey', 'hello', 'whats up', 'hows it going',
+                   'how are you', 'how have you been']
+
+# create library and add phrases
+phrases = {}
+phrases["morning_greet"] = (morning_greet)
+phrases["midday_greet"] = (midday_greet)
+phrases["afternoon_greet"] = (afternoon_greet)
+phrases["evening_greet"] = (evening_greet)
+phrases["night_greet"] = (night_greet)
+phrases["greet_followers"] = (greet_followers)
+phrases["greet_questions"] = (greet_questions)
+phrases["greetings"] = (greetings)
+
+# set user name
+user = "alex"
