5848c51e2c39cc19a5f406b7cfe40d3b0ded7bf8
ascherf-ml <alexander-scherf@gmx.net>
2020-11-10 09:02:44 +0100

    update

---
+++ modules/database.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import time
+
+def database_create():
+    main_data_columns = ['text', 'time', 'source', 'command']
+    main_data = pd.DataFrame(columns=main_data_columns)
+    todo_data_columns = ['todo', 'date', 'priority']
+    todo_data = pd.DataFrame(columns=todo_data_columns)
+    note_data_columns = ['note', 'date']
+    note_data = pd.DataFrame(columns=note_data_columns)
+
+def write(input, source, command, database, database_col):
+    data = pd.DataFrame([[input, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), source, command]], columns=database_col)
+    database= database.append(data)
+    return database
+
+
+def database_export():
+    main_data.to_csv('data/main_data.csv')
+
+
+def database_import():
+    main_data = pd.read_csv('data/main_data.csv',sep=',')
--- modules/speechprocessing.py
+++ modules/speechprocessing.py
@@ -5,16 +5,13 @@ import random
 import pandas as pd
 import time
 
+from modules.database import *
+
 
 def speak(text):
     engine = speech_engine()
     engine.say(text)
     engine.runAndWait()
-
-def write(input, source, command, database, database_col):
-    data = pd.DataFrame([[input, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), source, command]], columns=database_col)
-    database= database.append(data)
-    return database
 
 def takeCommand():
     r = sr.Recognizer()
@@ -106,11 +103,3 @@ phrases["greetings"] = (greetings)
 
 # set user name
 user = "alex"
-
-
-main_data_columns = ['text', 'time', 'source', 'command']
-main_data = pd.DataFrame(columns=main_data_columns)
-todo_data_columns = ['todo', 'date', 'priority']
-todo_data = pd.DataFrame(columns=todo_data_columns)
-note_data_columns = ['note', 'date']
-note_data = pd.DataFrame(columns=note_data_columns)
